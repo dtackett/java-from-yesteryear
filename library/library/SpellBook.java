4acaf2735de39126fbc0ad6782d3dbc650bff556
Devon Tackett <devon.tackett@gmail.com>
2013-01-08 21:42:32 -0700

    Basic example on creating and using a library

---
+++ library/library/SpellBook.java
@@ -0,0 +1,7 @@
+package library;
+
+public class SpellBook{
+	public String whatTimeIsIt() {
+		return "Adventure time!";
+	}
+}
---
+++ main/com/Adventurer.java
@@ -0,0 +1,11 @@
+package com;
+
+import library.SpellBook;
+
+public class Adventurer {
+	public static void main(String[] args) {
+		SpellBook book = new SpellBook();
+
+		System.out.println(book.whatTimeIsIt());
+	}
+}
