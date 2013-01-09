2daab2dd61e1d5ced0d340308019ec3a58b87af5
Devon Tackett <frogplague@Devon-Tacketts-MacBook-Pro.local>
2013-01-08 20:56:08 -0700

    Changed example to not use any imports (for simplicity sake)

--- ExceptionExample.java
+++ ExceptionExample.java
@@ -1,16 +1,13 @@
-import java.net.URISyntaxException;
-import java.net.URI;
-
 public class ExceptionExample {  
 
   public static void main(String[] args) {
     // Checked
-    //URI myURI = new URI("http://www.google.com");
+    // didSomethingBadHappen();
 
     try {
-      URI myURI = new URI("http://www.google.com fail!");
-    } catch (URISyntaxException ex) {
-      System.err.println("Invalid URI Syntax! ");
+      didSomethingBadHappen();
+    } catch (Exception ex) {
+      System.err.println("Error");
       ex.printStackTrace();
     }
 
@@ -20,5 +17,9 @@ public class ExceptionExample {
     // myString = "Hey there!";
 
     System.out.println(myString.toString());
+  }
+
+  public static void didSomethingBadHappen() throws Exception {
+    throw new Exception("Somethind bad happened!");
   }
 }
