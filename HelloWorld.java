3e30b796e48c977352da93946d9681fbdbf3bab5
Devon Tackett <devon.tackett@gmail.com>
2013-01-29 21:11:18 -0700

    Crude logging example

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    logger.info("Hello World");
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 java-from-yesteryear
 ====================
+
+Simple introduction to logging.
+
+http://slf4j.org/manual.html
+
