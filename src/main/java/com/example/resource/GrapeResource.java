93dbbc6209cb7f361218528b536ecaa2c82a753a
Devon Tackett <devon.tackett@meltmedia.com>
2013-03-20 14:06:42 -0700

    Expanded jersey example to include path params

--- src/main/java/com/example/resource/GrapeResource.java
+++ src/main/java/com/example/resource/GrapeResource.java
@@ -3,22 +3,32 @@ package com.example.resource;
 import com.example.data.Grape;
 
 import javax.ws.rs.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/grape")
 public class GrapeResource {
 
+    private static List<Grape> grapes = new ArrayList<Grape>();
+
     @GET
     @Produces("application/json")    
-    public Grape get() {
-        return new Grape();
+    public List<Grape> get() {
+        return grapes;
+    }
+
+    @GET
+    @Path("/{index}")
+    @Produces("application/json")
+    public Grape get(@PathParam("index") int index) {
+        return grapes.get(index);
     }
 
     @POST
     @Consumes("application/json")
     @Produces("application/json")
     public Grape post(Grape grape) {
-        grape.setWrangled(true);
-
+        grapes.add(grape);
         return grape;
     }
 
